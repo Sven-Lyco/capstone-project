f123845a7e28be8d70c003184ef3dab6b0f431b3
Sven Lüchow <sven.luechow@icloud.com>
2022-03-28 11:03:12 +0200

    Fetch data of popular Movies and popular Series

--- src/App.js
+++ src/App.js
@@ -1,4 +1,49 @@
+import { useState, useEffect } from 'react';
+
+const {
+  REACT_APP_API_BASE_SERIES_URL,
+  REACT_APP_API_BASE_MOVIES_URL,
+  REACT_APP_API_KEY,
+  REACT_APP_API_LANGUAGE,
+} = process.env;
+
+const popularSeriesUrl = `${REACT_APP_API_BASE_SERIES_URL}/popular?api_key=${REACT_APP_API_KEY}&language=${REACT_APP_API_LANGUAGE}`;
+const popularMoviesUrl = `${REACT_APP_API_BASE_MOVIES_URL}/popular?api_key=${REACT_APP_API_KEY}&language=${REACT_APP_API_LANGUAGE}&region=DE`;
+
 export default function App() {
+  const [popularSeries, setPopularSeries] = useState([]);
+  const [popularMovies, setPopularMovies] = useState([]);
+  console.log({ popularSeries });
+  console.log({ popularMovies });
+
+  useEffect(() => {
+    /* ---LOAD POPULAR SERIES */
+    async function loadPopularSeries() {
+      try {
+        const response = await fetch(popularSeriesUrl);
+        const data = await response.json();
+        setPopularSeries(data.results);
+      } catch (error) {
+        console.error(error);
+      }
+    }
+    loadPopularSeries();
+  }, []);
+
+  useEffect(() => {
+    /* ---LOAD POPULAR MOVIES */
+    async function loadPopularMovies() {
+      try {
+        const response = await fetch(popularMoviesUrl);
+        const data = await response.json();
+        setPopularMovies(data.results);
+      } catch (error) {
+        console.error(error);
+      }
+    }
+    loadPopularMovies();
+  }, []);
+
   return (
     <>
       <h1>capstone-project</h1>
