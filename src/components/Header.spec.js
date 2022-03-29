8425ceb425ab093ffaf46dcd5f69d5d02dedcca1
Sven Lüchow <sven.luechow@icloud.com>
2022-03-29 15:32:39 +0200

    Change Specs for Header

--- src/components/Header.spec.js
+++ src/components/Header.spec.js
@@ -1,18 +1,37 @@
-import { render, screen } from '@testing-library/react';
 import Header from './Header';
+import { MemoryRouter } from 'react-router-dom';
+import { render, screen } from '@testing-library/react';
 
 describe('Header', () => {
   it('renders a Header', () => {
-    render(<Header alt="Header" />);
+    render(
+      <MemoryRouter>
+        <Header alt="Header" />
+      </MemoryRouter>
+    );
 
     const header = screen.getByRole('heading');
     expect(header).toBeInTheDocument();
   });
 
   it('has an alt text', () => {
-    render(<Header alt="Header" />);
+    render(
+      <MemoryRouter>
+        <Header alt="Header" />
+      </MemoryRouter>
+    );
 
     const header = screen.getByAltText('Header');
     expect(header).toHaveAttribute('alt', 'Header');
+  });
+
+  it('has one nav link', () => {
+    render(
+      <MemoryRouter>
+        <Header />
+      </MemoryRouter>
+    );
+    const links = screen.getAllByRole('link');
+    expect(links.length).toBeGreaterThan(0);
   });
 });
