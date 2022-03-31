06fc7626d4cdc95157da86f6c74528c5ef0b4b11
Sven Lüchow <sven.luechow@icloud.com>
2022-03-31 19:44:39 +0200

    Create Test for NotFound Page

---
+++ src/pages/NotFound.spec.js
@@ -0,0 +1,19 @@
+import { MemoryRouter } from 'react-router-dom';
+import { render, screen } from '@testing-library/react';
+import NotFound from './NotFound';
+
+describe('Navigation', () => {
+  it('renders the Page with heading and one link', () => {
+    render(
+      <MemoryRouter>
+        <NotFound />
+      </MemoryRouter>
+    );
+
+    const heading = screen.getByRole('heading', { level: 1 });
+    const backLink = screen.getByRole('link', { name: /zurück/i });
+
+    expect(heading).toBeInTheDocument();
+    expect(backLink).toBeInTheDocument();
+  });
+});
