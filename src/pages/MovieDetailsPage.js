b474de88099b449bf74c09404fd756bdfdee5afe
Sven Lüchow <sven.luechow@icloud.com>
2022-03-31 14:46:31 +0200

    Finished MoveDetailsPage

---
+++ src/pages/MovieDetailsPage.js
@@ -0,0 +1,136 @@
+import { useParams, Link } from 'react-router-dom';
+import styled from 'styled-components';
+import useDetails from '../hooks/useDetails';
+import Poster from '../components/Poster';
+import ScreenReaderOnly from '../components/ScreenReaderOnly';
+import { ReactComponent as ArrowBackIcon } from '../assets/icons/arrow_back.svg';
+
+const {
+  REACT_APP_API_BASE_MOVIES_URL,
+  REACT_APP_API_KEY,
+  REACT_APP_API_LANGUAGE,
+} = process.env;
+
+export default function SeriesDetailsPage() {
+  const { id } = useParams();
+  const movieDetailsUrl = `${REACT_APP_API_BASE_MOVIES_URL}/${id}?api_key=${REACT_APP_API_KEY}&language=${REACT_APP_API_LANGUAGE}`;
+  const { data: movie } = useDetails(movieDetailsUrl);
+
+  console.log(movie);
+
+  return (
+    <Wrapper>
+      <StyledLinkBack to="/filme">
+        <StyledArrowBackIcon />
+        <ScreenReaderOnly>Zurück</ScreenReaderOnly>
+      </StyledLinkBack>
+      <StyledBackdropImage backdropPath={movie.backdrop_path} />
+      <StyledHeader>
+        <Poster
+          src={
+            movie.poster_path
+              ? `https://image.tmdb.org/t/p/w300${movie.poster_path}`
+              : require('../assets/images/poster.png')
+          }
+          alt={`${movie.title}`}
+        />
+        <StyledHeaderBox>
+          <StyledTitle>{movie.title}</StyledTitle>
+          <p>
+            {movie.release_date
+              ? movie.release_date.substr(0, 4)
+              : 'kein Release Datum vorhanden'}{' '}
+            - {calcMovieTime(movie.runtime)}
+          </p>
+        </StyledHeaderBox>
+      </StyledHeader>
+      <StyledMain>
+        <h3>Handlung</h3>
+        <p>
+          {movie.overview
+            ? movie.overview
+            : 'Aktuell ist leider keine Beschreibung verfügbar'}
+        </p>
+      </StyledMain>
+    </Wrapper>
+  );
+
+  function calcMovieTime(movieRunTime) {
+    const hours = Math.floor(movieRunTime / 60);
+    const minutes = Math.ceil((movieRunTime / 60 - hours) * 60);
+    return `${hours} Std. ${minutes} min.`;
+  }
+}
+
+const Wrapper = styled.div`
+  position: relative;
+  width: 100%;
+`;
+
+const StyledBackdropImage = styled.div`
+  z-index: -1;
+  position: relative;
+  @media (min-width: 576px) {
+    min-height: 360px;
+  }
+  min-height: 300px;
+  height: 100%;
+  background: ${({ backdropPath }) =>
+      backdropPath ? `url(https://image.tmdb.org/t/p/w780${backdropPath})` : ''}
+    center 0 no-repeat;
+  background-size: cover;
+  background-position: center;
+  margin-bottom: -60px;
+  box-shadow: inset 0px -65px 50px 0px var(--color-black);
+`;
+
+const StyledLinkBack = styled(Link)`
+  display: flex;
+  align-items: center;
+  align-self: flex-start;
+  position: absolute;
+  top: 12px;
+  left: 12px;
+  color: var(--color-green);
+`;
+
+const StyledArrowBackIcon = styled(ArrowBackIcon)`
+  background-color: rgba(18, 18, 18, 0.4);
+  border-radius: var(--border-radius);
+`;
+
+const StyledHeader = styled.header`
+  display: flex;
+  max-height: 180px;
+  margin-left: 20px;
+`;
+
+const StyledTitle = styled.span`
+  font-size: xx-large;
+  font-weight: bold;
+  margin: 0;
+  padding: 25px 0px 5px;
+`;
+
+const StyledHeaderBox = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: flex-start;
+  justify-content: flex-start;
+  padding: 0;
+  margin: 10px;
+  width: 100%;
+
+  p {
+    font-size: larger;
+    font-style: italic;
+    font-weight: 400;
+    margin: 0;
+    padding: 0;
+  }
+`;
+
+const StyledMain = styled.main`
+  margin: 20px;
+  padding: 0;
+`;
