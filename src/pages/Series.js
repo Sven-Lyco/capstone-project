dd6f091bba4a4143cf5654ec576194c077abba1b
Sven Lüchow <sven.luechow@icloud.com>
2022-04-04 11:41:27 +0200

    Add useCheckFetch to SeriesPage

--- src/pages/Series.js
+++ src/pages/Series.js
@@ -2,17 +2,29 @@ import styled from 'styled-components';
 import Header from '../components/Header';
 import PosterList from '../components/PosterList';
 import Navigation from '../components/Navigation';
+import FetchError from '../components/FetchError';
+import useCheckFetch from '../hooks/useCheckFetch';
 
 export default function Series({ popularSeries, topRatedSeries, seriesOnTv }) {
+  const { data } = useCheckFetch(popularSeries, topRatedSeries, seriesOnTv);
+
   return (
     <Wrapper>
       <Header />
-      <main>
-        <PosterList list={popularSeries} listName={'Beliebte Serien'} />
-        <PosterList list={topRatedSeries} listName={'Top bewertete Serien'} />
-        <PosterList list={seriesOnTv} listName={'Serien im TV'} />
-      </main>
-      <Navigation />
+      {!data && <FetchError />}
+      {data && (
+        <>
+          <main>
+            <PosterList list={popularSeries} listName={'Beliebte Serien'} />
+            <PosterList
+              list={topRatedSeries}
+              listName={'Top bewertete Serien'}
+            />
+            <PosterList list={seriesOnTv} listName={'Serien im TV'} />
+          </main>
+          <Navigation />
+        </>
+      )}
     </Wrapper>
   );
 }
