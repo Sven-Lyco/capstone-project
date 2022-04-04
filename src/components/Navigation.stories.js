26d8ef73532e4856e67f92f2c686596753c0d514
Sven Lüchow <sven.luechow@icloud.com>
2022-04-04 11:50:26 +0200

    Update navigation stories

--- src/components/Navigation.stories.js
+++ src/components/Navigation.stories.js
@@ -3,7 +3,6 @@ import Navigation from './Navigation';
 export default {
   title: 'components/Navigation',
   component: Navigation,
-  argTypes: {},
 };
 
 const Template = args => <Navigation {...args} />;
