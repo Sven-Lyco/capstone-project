5ad6e3b4762f74fbf0efd8390cea3b966c151a2c
Sven Lüchow <sven.luechow@icloud.com>
2022-04-04 11:29:00 +0200

    Create FetchError Component

---
+++ src/components/FetchError.js
@@ -0,0 +1,43 @@
+import { GoSync } from 'react-icons/go';
+import styled from 'styled-components';
+
+export default function NotFound() {
+  return (
+    <Wrapper>
+      <h1>ERROR</h1>
+      <span>Irgendetwas ist funktioniert nicht</span>
+      <span>wie es sollte</span>
+      <StyledButton onClick={() => window.location.reload(false)}>
+        <GoSync />
+        <span>Seite neu laden</span>
+      </StyledButton>
+    </Wrapper>
+  );
+}
+
+const Wrapper = styled.div`
+  display: flex;
+  flex-wrap: wrap;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  height: 80vh;
+
+  span {
+    text-align: center;
+  }
+`;
+
+const StyledButton = styled.div`
+  display: flex;
+  align-items: center;
+  align-self: center;
+  height: 50px;
+  text-decoration: none;
+  background-color: var(--color-light-gray);
+  color: var(--color-black);
+  margin: 15px;
+  padding: 15px;
+  border-radius: 10px;
+  cursor: pointer;
+`;
