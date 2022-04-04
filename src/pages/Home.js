1732d0b41a66ad99e32f0c331176a0c6e63739ce
Sven Lüchow <sven.luechow@icloud.com>
2022-04-04 11:29:16 +0200

    Add Fetch Error to App

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -2,16 +2,25 @@ import styled from 'styled-components';
 import Header from '../components/Header';
 import PosterList from '../components/PosterList';
 import Navigation from '../components/Navigation';
+import FetchError from '../components/FetchError';
+import useCheckFetch from '../hooks/useCheckFetch';
 
 export default function Home({ popularSeries, popularMovies }) {
+  const { data } = useCheckFetch(popularSeries, popularMovies);
+
   return (
     <Wrapper>
       <Header />
-      <main>
-        <PosterList list={popularSeries} listName={'Beliebte Serien'} />
-        <PosterList list={popularMovies} listName={'Beliebte Filme'} />
-      </main>
-      <Navigation />
+      {!data && <FetchError />}
+      {data && (
+        <>
+          <main>
+            <PosterList list={popularSeries} listName={'Beliebte Serien'} />
+            <PosterList list={popularMovies} listName={'Beliebte Filme'} />
+          </main>
+          <Navigation />
+        </>
+      )}
     </Wrapper>
   );
 }
