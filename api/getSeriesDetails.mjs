7a72f142e62dbb5e2d343f3626b560983a257f5d
Sven Lüchow <sven.luechow@icloud.com>
2022-04-05 16:12:40 +0200

    Update mjs for seriesDetails

--- api/getSeriesDetails.mjs
+++ api/getSeriesDetails.mjs
@@ -8,8 +8,8 @@ const {
 
 export default async function getSeriesDetails(req, res) {
   if (req.method === 'GET') {
-    //const { id } = req.params;
-    const seriesDetailsUrl = `${REACT_APP_API_BASE_SERIES_URL}/1399?api_key=${REACT_APP_API_KEY}&language=${REACT_APP_API_LANGUAGE}`;
+    const { id } = req.params;
+    const seriesDetailsUrl = `${REACT_APP_API_BASE_SERIES_URL}/${id}?api_key=${REACT_APP_API_KEY}&language=${REACT_APP_API_LANGUAGE}`;
     const fetchResponse = await fetch(seriesDetailsUrl);
     const data = await fetchResponse.json();
     res.status(200).json(data);
