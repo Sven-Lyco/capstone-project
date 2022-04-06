51cc903aaa4c9cd4176895f6f25f2784e00971d3
Sven Lüchow <sven.luechow@icloud.com>
2022-04-06 08:55:00 +0200

    Update fetch for series and movie details

--- api/getMovieDetails.mjs
+++ api/getMovieDetails.mjs
@@ -4,7 +4,8 @@ const { API_BASE_MOVIES_URL, API_KEY, API_LANGUAGE } = process.env;
 
 export default async function getSeriesDetails(req, res) {
   if (req.method === 'POST') {
-    const seriesDetailsUrl = `${API_BASE_MOVIES_URL}/${req.body}?api_key=${API_KEY}&language=${API_LANGUAGE}`;
+    const id = req.body;
+    const seriesDetailsUrl = `${API_BASE_MOVIES_URL}/${id}?api_key=${API_KEY}&language=${API_LANGUAGE}`;
     const fetchResponse = await fetch(seriesDetailsUrl);
     const data = await fetchResponse.json();
     res.status(200).json(data);
