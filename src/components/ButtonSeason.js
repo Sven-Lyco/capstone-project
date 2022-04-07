d40a8ca249d9caadb936b0b67f73fd2c3131b02e
Sven Lüchow <sven.luechow@icloud.com>
2022-04-07 16:01:58 +0200

    Write Spec for ButtonSeason

--- src/components/ButtonSeason.js
+++ src/components/ButtonSeason.js
@@ -10,10 +10,10 @@ export default function ButtonSeason({ name, onClick, isActive }) {
 const StyledButton = styled.button`
   white-space: nowrap;
   padding: 5px;
-  border: 2px solid var(--color-orange);
+  border: 2px solid var(--color-light-gray);
   border-radius: var(--border-radius);
   font-size: large;
   color: ${({ isActive }) => (isActive ? `var(--color-black)` : `inherit`)};
   background-color: ${({ isActive }) =>
-    isActive ? `var(--color-orange)` : `transparent`};
+    isActive ? `var(--color-light-gray)` : `transparent`};
 `;
---
+++ src/components/ButtonSeason.spec.js
@@ -0,0 +1,46 @@
+import ButtonSeason from './ButtonSeason';
+import { render, screen } from '@testing-library/react';
+import userEvent from '@testing-library/user-event';
+
+describe('ButtonWatchlist', () => {
+  it('renders', () => {
+    render(
+      <ButtonSeason
+        onClick={() => jest.fn()}
+        isActive={true}
+        name={'Staffel 1'}
+      />
+    );
+
+    const button = screen.getByRole('button');
+    expect(button).toBeInTheDocument();
+  });
+
+  it('has onClick function', () => {
+    const handleOnClick = jest.fn();
+    render(
+      <ButtonSeason
+        onClick={handleOnClick}
+        isActive={true}
+        name={'Staffel 1'}
+      />
+    );
+
+    const button = screen.getByRole('button');
+    userEvent.click(button);
+    expect(handleOnClick).toHaveBeenCalled();
+  });
+
+  it('has the correct button text', () => {
+    render(
+      <ButtonSeason
+        onClick={() => jest.fn()}
+        isActive={true}
+        name={'Staffel 1'}
+      />
+    );
+
+    const text = screen.getByText('Staffel 1');
+    expect(text).toBeInTheDocument();
+  });
+});
