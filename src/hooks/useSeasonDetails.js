59a4010e8c2819f153f401a03db6e78f429d7bc1
Sven Lüchow <sven.luechow@icloud.com>
2022-04-07 15:36:07 +0200

    create hook for SeasonDetails

---
+++ src/hooks/useSeasonDetails.js
@@ -0,0 +1,28 @@
+import { useState, useEffect } from 'react';
+
+export default function useSeasonDetails(url) {
+  const [seasonDetails, setSeasonDetails] = useState([]);
+  const [isLoading, setIsLoading] = useState(false);
+
+  useEffect(() => {
+    setIsLoading(true);
+    async function loadSeason() {
+      try {
+        const response = await fetch('/api/getSeason/', {
+          method: 'POST',
+          headers: {
+            'Content-Type': 'application/json',
+          },
+          body: JSON.stringify(url),
+        });
+        const data = await response.json();
+        setSeasonDetails(data);
+      } catch (error) {
+        console.error(error);
+      }
+      setIsLoading(false);
+    }
+    loadSeason();
+  }, [url]);
+  return { seasonDetails, isLoading };
+}
