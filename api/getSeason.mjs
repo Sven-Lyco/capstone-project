f35886ca655a7118f4b2a2e87af6c91484e05452
Sven Lüchow <sven.luechow@icloud.com>
2022-04-07 15:35:14 +0200

    Update fetch

--- api/getSeason.mjs
+++ api/getSeason.mjs
@@ -4,8 +4,8 @@ const { API_BASE_SERIES_URL, API_KEY, API_LANGUAGE } = process.env;
 
 export default async function getSeason(req, res) {
   if (req.method === 'POST') {
-    const { seriesId, currentSeasonNumber } = req.body;
-    const seriesDetailsUrl = `${API_BASE_SERIES_URL}/${seriesId}/season/${currentSeasonNumber}?api_key=${API_KEY}&language=${API_LANGUAGE}`;
+    const fetchURL = req.body;
+    const seriesDetailsUrl = `${API_BASE_SERIES_URL}/${fetchURL}?api_key=${API_KEY}&language=${API_LANGUAGE}`;
     const fetchResponse = await fetch(seriesDetailsUrl);
     const data = await fetchResponse.json();
     res.status(200).json(data);
