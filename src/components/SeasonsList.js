298c0ee13677ee6cad4f3a837e73f27327e5fb4b
Sven Lüchow <sven.luechow@icloud.com>
2022-04-07 15:38:23 +0200

    Finished SeasonsList

--- src/components/SeasonsList.js
+++ src/components/SeasonsList.js
@@ -1,41 +1,54 @@
 import { useState } from 'react';
 import styled from 'styled-components';
-import LoadingSpinner from './LoadingSpinner';
 import ButtonSeason from './ButtonSeason';
+import Poster from './Poster';
+import useSeasonDetails from '../hooks/useSeasonDetails';
+import EpisodeCard from './EpisodeCard';
 
-export default function SeasonsList({ seasons }) {
+export default function SeasonsList({ seasons, seriesId }) {
   const [currentSeasonNumber, setCurrentSeasonNumber] = useState(1);
   const currentSeason = seasons?.find(
     ({ season_number: seasonNumber }) => seasonNumber === currentSeasonNumber
   );
-
-  console.log(currentSeasonNumber);
+  const fetchURL = `${seriesId}/season/${currentSeasonNumber}`;
+  const { seasonDetails } = useSeasonDetails(fetchURL);
+  const { poster_path: posterPath, name } = currentSeason;
+  const seasonEpisodes = seasonDetails.episodes;
 
   return (
-    <>
-      {seasons ? (
-        <StyledList role="list">
-          {seasons
-            .filter(result => result.name !== 'Extras')
-            .map(({ id, name, season_number: seasonNumber }) => (
-              <li key={id}>
-                <ButtonSeason
-                  name={name}
-                  isActive={seasonNumber === currentSeasonNumber}
-                  onClick={() => handleSwitchSeason(seasonNumber)}
-                >
-                  {name}
-                </ButtonSeason>
-              </li>
-            ))}
-        </StyledList>
-      ) : (
-        <>
-          <LoadingSpinner />
-        </>
-      )}
-      <>{console.log(currentSeason)}</>
-    </>
+    <section>
+      <StyledList role="list">
+        {seasons
+          .filter(result => result.name !== 'Extras')
+          .map(({ id, name, season_number: seasonNumber }) => (
+            <ButtonSeason
+              key={id}
+              name={name}
+              isActive={seasonNumber === currentSeasonNumber}
+              onClick={() => handleSwitchSeason(seasonNumber)}
+            />
+          ))}
+      </StyledList>
+      <InfoWrapper>
+        <Poster
+          src={
+            posterPath
+              ? `https://image.tmdb.org/t/p/w300${posterPath}`
+              : require('../assets/images/poster.png')
+          }
+          alt={name}
+        />
+        <TextBox>
+          <p>Staffel {currentSeasonNumber}</p>
+          <p>{seasonEpisodes?.length} Episoden</p>
+        </TextBox>
+      </InfoWrapper>
+      <div>
+        {seasonEpisodes?.map(episode => (
+          <EpisodeCard key={episode.id} episode={episode} />
+        ))}
+      </div>
+    </section>
   );
 
   function handleSwitchSeason(seasonNumber) {
@@ -46,8 +59,26 @@ export default function SeasonsList({ seasons }) {
 const StyledList = styled.ul`
   list-style: none;
   padding: 0 0 0 20px;
+  margin: 0;
   display: flex;
   gap: 10px;
   overflow-x: auto;
   overflow-y: hidden;
+`;
+
+const InfoWrapper = styled.div`
+  display: flex;
+  padding: 20px;
+`;
+
+const TextBox = styled.div`
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  padding: 0 20px;
+  margin: 0;
+
+  p {
+    font-size: larger;
+  }
 `;
