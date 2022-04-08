4deec355d74a7ae56a92664c8235cfaeef2794d0
Sven Lüchow <sven.luechow@icloud.com>
2022-04-08 23:12:55 +0200

    Update Button.stories.js

--- src/components/Button.stories.js
+++ src/components/Button.stories.js
@@ -10,24 +10,24 @@ const Template = args => <Button {...args} />;
 
 export const Default = Template.bind({});
 Default.args = {
-  text: 'click me',
+  children: 'click me',
   buttonType: 'normal',
 };
 
 export const NeueFische = Template.bind({});
 NeueFische.args = {
-  text: 'click me',
+  children: 'click me',
   buttonType: 'neueFische',
 };
 
 export const GreenButton = Template.bind({});
 GreenButton.args = {
-  text: 'click me',
+  children: 'click me',
   buttonType: 'green',
 };
 
 export const RedButton = Template.bind({});
 RedButton.args = {
-  text: 'click me',
+  children: 'click me',
   buttonType: 'red',
 };
