873844417bdfc8541953922a2b902b307afe0383
Sven Lüchow <sven.luechow@icloud.com>
2022-04-09 17:09:45 +0200

    Create CastList component with specs and storybook

---
+++ src/components/CastList.js
@@ -0,0 +1,63 @@
+import styled from 'styled-components';
+import PosterActor from './PosterActor';
+import defaultPoster from '../assets/images/profile.png';
+
+export default function CastList({ seriesCast, listName }) {
+  return (
+    <>
+      <ListHeader>{listName}</ListHeader>
+      <StyledList role="list">
+        {seriesCast?.map(({ id, name, character, profile_path }) => (
+          <li key={id}>
+            <ImageBox>
+              <PosterActor
+                src={
+                  profile_path
+                    ? `https://image.tmdb.org/t/p/w300${profile_path}`
+                    : defaultPoster
+                }
+                alt={name}
+              />
+              <ActorName>{name}</ActorName>
+              <CharacterName>{character}</CharacterName>
+            </ImageBox>
+          </li>
+        ))}
+      </StyledList>
+    </>
+  );
+}
+
+const ListHeader = styled.h3`
+  margin: 10px 0px 0px 0px;
+`;
+
+const StyledList = styled.ul`
+  list-style: none;
+  padding: 0 20px;
+  margin: 0px -20px;
+  display: grid;
+  grid-template-columns: repeat(10, auto);
+  grid-template-rows: 1fr;
+  gap: 20px;
+  overflow-x: auto;
+  overflow-y: hidden;
+
+  li {
+    padding: 0;
+    margin: 0;
+  }
+`;
+
+const ImageBox = styled.div`
+  position: relative;
+`;
+
+const ActorName = styled.p`
+  color: var(--color-white);
+`;
+
+const CharacterName = styled.p`
+  font-size: small;
+  color: var(--color-light-gray);
+`;
---
+++ src/components/CastList.stories.js
@@ -0,0 +1,26 @@
+import CastList from './CastList';
+
+export default {
+  title: 'components/CastList',
+  component: CastList,
+};
+
+const Template = args => <CastList {...args} />;
+
+export const Default = Template.bind({});
+Default.args = {
+  listName: 'Listen Name',
+
+  seriesCast: [
+    { id: 1, name: 'Title 1', character: 'Character 1' },
+    { id: 2, name: 'Title 2', character: 'Character 2' },
+    { id: 3, name: 'Title 3', character: 'Character 3' },
+    { id: 4, name: 'Title 4', character: 'Character 4' },
+    { id: 5, name: 'Title 5', character: 'Character 5' },
+    { id: 6, name: 'Title 6', character: 'Character 6' },
+    { id: 7, name: 'Title 7', character: 'Character 7' },
+    { id: 8, name: 'Title 8', character: 'Character 8' },
+    { id: 9, name: 'Title 9', character: 'Character 9' },
+    { id: 10, name: 'Title 10', character: 'Character 10' },
+  ],
+};
