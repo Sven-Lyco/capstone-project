5ef0c50b2e10b7685a8d17a69096fc21d7d8f139
Sven Lüchow <sven.luechow@icloud.com>
2022-04-11 19:57:04 +0200

    Update mongoose Schema for correct GET and POST request

--- api/watchlist/index.mjs
+++ api/watchlist/index.mjs
@@ -1,4 +1,4 @@
-import WatchlistEntry from '../../backend/models/WatchlistEntry.mjs';
+import Watchlist from '../../backend/models/Watchlist.mjs';
 import connectToMongoDB from '../../backend/db/dbConnect.mjs';
 
 await connectToMongoDB();
@@ -7,12 +7,12 @@ export default async function handler(req, res) {
   console.log('connected to MongoDB');
 
   if (req.method === 'GET') {
-    const watchlist = await WatchlistEntry.find();
+    const watchlist = await Watchlist.find();
     return res.status(200).json(watchlist);
   }
 
   if (req.method === 'POST') {
-    const newWatchlistEntry = await WatchlistEntry.create(req.body);
+    const newWatchlistEntry = await Watchlist.create(req.body);
     return res.status(200).json(newWatchlistEntry);
   }
 
--- backend/models/Watchlist.mjs
+++ backend/models/Watchlist.mjs
@@ -10,6 +10,7 @@ const schema = new Schema(
   },
   {
     versionKey: false,
+    collection: 'watchlist',
   }
 );
 
