27bd41d379819b10f416ebc8334cb24128448d4d
Sven Lüchow <sven.luechow@icloud.com>
2022-04-12 08:45:33 +0200

    Create function to add movie to watchlist

--- src/hooks/useWatchlist.js
+++ src/hooks/useWatchlist.js
@@ -3,19 +3,30 @@ import useSWR from 'swr';
 const fetcher = (...args) => fetch(...args).then(res => res.json());
 
 export default function useWatchlist() {
-  const { data: watchlist, error: watchlistError } = useSWR(
-    '../../api/watchlist',
-    fetcher
-  );
+  const {
+    data: watchlist,
+    error: watchlistError,
+    mutate: mutateWatchlist,
+  } = useSWR('../../api/watchlist', fetcher);
 
-  // function handleAddMovie(id, title, posterPath) {
-  //   const watchlistItem = { id, title, posterPath };
-  //   if (watchlist.find(item => item.id === watchlistItem.id)) {
-  //     setWatchlist([...watchlist]);
-  //   } else {
-  //     setWatchlist([watchlistItem, ...watchlist]);
-  //   }
-  // }
+  console.log(watchlist);
+
+  async function handleAddMovie(id, title, posterPath) {
+    const watchlistItem = { id, title, posterPath };
+    if (watchlist.find(item => item.id === watchlistItem.id)) {
+      mutateWatchlist([...watchlist]);
+    } else {
+      mutateWatchlist([...watchlist, watchlistItem], false);
+      await fetch('/api/watchlist', {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify(watchlistItem),
+      });
+      mutateWatchlist();
+    }
+  }
 
   // function handleAddSeries(id, name, posterPath) {
   //   const watchlistItem = { id, name, posterPath };
@@ -42,8 +53,8 @@ export default function useWatchlist() {
     watchlist,
     watchlistError,
     checkIsOnWatchlist,
-    // handleDeleteItem,
-    // handleAddSeries,
-    // handleAddMovie,
+    //handleDeleteItem,
+    //handleAddSeries,
+    handleAddMovie,
   };
 }
