2e2f84811e77184593e3a9c5c293ed429d2a738e
Sven Lüchow <sven.luechow@icloud.com>
2022-04-12 15:10:59 +0200

    Add border-radius to ButtonCheckMovie

--- src/components/ButtonCheckMovie.js
+++ src/components/ButtonCheckMovie.js
@@ -31,6 +31,7 @@ const StyledButton = styled.button`
   color: ${({ isMovieWatched }) =>
     isMovieWatched ? 'var(--color-green)' : 'var(--color-light-gray)'};
   border: none;
+  border-radius: 50%;
   cursor: pointer;
 `;
 
