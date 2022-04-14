dace6822cfda338a8f9c5f7407112ca639eb0e59
Sven Lüchow <sven.luechow@icloud.com>
2022-04-14 12:17:16 +0200

    Write specs for ToggleSwitch

---
+++ src/components/ToggleSwitch.spec.js
@@ -0,0 +1,21 @@
+import ToggleSwitch from './ToggleSwitch';
+import { render, screen } from '@testing-library/react';
+import userEvent from '@testing-library/user-event';
+
+describe('ToggleSwitch', () => {
+  it('renders a checkbox as a toggle switch', () => {
+    render(<ToggleSwitch />);
+
+    const checkbox = screen.getByRole('checkbox');
+    expect(checkbox).toBeInTheDocument();
+  });
+
+  it('has onChange function', () => {
+    const handleOnChange = jest.fn();
+    render(<ToggleSwitch onChange={handleOnChange} />);
+
+    const checkbox = screen.getByRole('checkbox');
+    userEvent.click(checkbox);
+    expect(handleOnChange).toHaveBeenCalled();
+  });
+});
