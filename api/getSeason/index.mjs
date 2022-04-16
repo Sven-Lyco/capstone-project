d98cc6310c99642e343d0c8391bd410b4d9bbb14
Sven Lüchow <sven.luechow@icloud.com>
2022-04-16 17:59:05 +0200

    Rename files in api folder

--- api/getSeason/index.mjs
+++ api/getSeason/index.mjs
@@ -5,8 +5,8 @@ const { API_BASE_SERIES_URL, API_KEY, API_LANGUAGE } = process.env;
 export default async function getSeason(req, res) {
   if (req.method === 'POST') {
     const fetchURL = req.body;
-    const seriesDetailsUrl = `${API_BASE_SERIES_URL}/${fetchURL}?api_key=${API_KEY}&language=${API_LANGUAGE}`;
-    const fetchResponse = await fetch(seriesDetailsUrl);
+    const seasonDetailsUrl = `${API_BASE_SERIES_URL}/${fetchURL}?api_key=${API_KEY}&language=${API_LANGUAGE}`;
+    const fetchResponse = await fetch(seasonDetailsUrl);
     const data = await fetchResponse.json();
     res.status(200).json(data);
     return;
