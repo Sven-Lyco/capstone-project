4e6c1c83138caf40d1d05b4d23220ff31f48a759
Sven Lüchow <sven.luechow@icloud.com>
2022-04-18 11:46:46 +0200

    Update VideoFrame.stories.js

--- src/components/VideoFrame.stories.js
+++ src/components/VideoFrame.stories.js
@@ -9,7 +9,6 @@ const Template = args => <VideoFrame {...args} />;
 
 export const Default = Template.bind({});
 Default.args = {
-  showTrailer: true,
   videoUrl: [
     'https://www.youtube.com/watch?v=N3ss_-luLhE',
     'https://www.youtube.com/watch?v=bV59rGlNc6Y',
