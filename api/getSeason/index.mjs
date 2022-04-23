c9a53516e15b8973473da2102aaaac5bf88cce8f
Sven Lüchow <sven.luechow@icloud.com>
2022-04-23 13:25:56 +0200

    Update api files

--- api/getSeason/index.mjs
+++ api/getSeason/index.mjs
@@ -6,6 +6,7 @@ export default async function getSeason(req, res) {
   if (req.method === 'POST') {
     // To test POST request with _getSeason.http use: const { fetchUrl } = req.body;
     const fetchUrl = req.body;
+
     const seasonDetailsUrl = `${API_BASE_SERIES_URL}/${fetchUrl}?api_key=${API_KEY}&language=${API_LANGUAGE}`;
     const fetchResponse = await fetch(seasonDetailsUrl);
     const data = await fetchResponse.json();
