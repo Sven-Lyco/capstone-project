2f7d893960d17a68b9924c086dcabc6ed808145b
Sven Lüchow <sven.luechow@icloud.com>
2022-04-23 14:34:00 +0200

    Build BackdropImage component with storybook

---
+++ src/components/BackdropImage.js
@@ -0,0 +1,20 @@
+import styled from 'styled-components';
+
+export default styled.div`
+  z-index: -1;
+  position: relative;
+  @media (min-width: 576px) {
+    min-height: ${({ backdropPath }) => (backdropPath ? `360px` : `140px`)};
+  }
+  min-height: ${({ backdropPath }) => (backdropPath ? `300px` : `140px`)};
+  height: 100%;
+  background: ${({ backdropPath }) =>
+      backdropPath
+        ? `url(https://image.tmdb.org/t/p/original${backdropPath})`
+        : ''}
+    center 0 no-repeat;
+  background-size: cover;
+  background-position: center;
+  margin-bottom: -60px;
+  box-shadow: inset 0 -65px 50px 0 var(--color-black);
+`;
